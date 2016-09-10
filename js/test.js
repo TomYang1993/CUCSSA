dbae611d62ab161a9d1bc5dc14ef8e7868f702fb
Tom Yang <xy2286@columbia.edu>
2016-09-10 08:46:30 -0400

    initial commit

---
+++ js/test.js
@@ -0,0 +1,12 @@
+console.log('hello!');
+
+// Initialize collapse button
+  $(".button-collapse").sideNav();
+  // Initialize collapsible (uncomment the line below if you use the dropdown variation)
+  $('.collapsible').collapsible();
+
+  $(".dropdown-button").dropdown();
+
+
+
+  $("body.side-nav .dropdown-button").dropdown();
